a50744e1535215238afd0014ac88288aa71d035c
plrnvld <paulrijneveld@gmail.com>
2022-02-13 13:04:46 +0000

    Use more caching, example still wrong

--- main.scala
+++ main.scala
@@ -4,7 +4,7 @@ import scala.annotation.switch
 
 object Main {
     def main(args: Array[String]): Unit = {
-        val lines = Source.fromFile("Input.txt").getLines.toList
+        val lines = Source.fromFile("Example.txt").getLines.toList
         println(s"Lines count: ${lines.size}")
 
         val bootRules = lines.map(parse)
@@ -13,44 +13,67 @@ object Main {
 
         println(s"$minX <= x <= $maxX, $minY <= y <= $maxY, $minZ <= z <= $maxZ")
 
-        var onCount = 0;
-
+        var onCount = 0L;
         var activeRules = bootRules
+        var prevResult = (Seq[BootRuleSlice](), 0L)
 
         for (x <- minX to maxX) {
-            if (x % 1000 == 0)
-                println(s"Calculate slice for x = $x")
-                
             activeRules = bootRules.filter(b => x >= b.minX && x <= b.maxX)          
 
-            val activeSlices = activeRules.map(_.sliceYZ())
-            onCount += slicesOnCount(x, activeSlices)
+            if (!activeRules.isEmpty) {
+                val activeSlices = activeRules.map(_.sliceYZ())
+
+                val score = slicesScore(x, activeSlices, prevResult)
+                prevResult = (activeSlices, score)
+
+                onCount += score
+            }
         }
 
         println(s"On count = $onCount")
     }
 
-    def slicesOnCount(sliceNum: Int, activeSlices: Seq[BootRuleSlice]): Int = {
-        val switchOn = ruleOutcome(1, 2, activeSlices)   
-        
-        if (activeSlices.size == 1) {
-            if (switchOn) {
-                throw new Exception("Does this even happen: " + sliceNum)
-                1
-            }
-            else {
-                0
-            }
+    def slicesScore(sliceNum: Int, activeSlices: Seq[BootRuleSlice], prevResult: (Seq[BootRuleSlice], Long) ): Long = {
+        if (activeSlices == prevResult._1) {
+            prevResult._2
         } else {
-            println(s"  x = $sliceNum has ${activeSlices.size} slices")
+            val border = slicesBorder(activeSlices)
 
-            if (switchOn) {
-                activeSlices.size
+            var score = 0L
+            println(s"Calculate slice for x = $sliceNum")
+            println("[                                        ]")
+            print(" ")
+
+            val widthPart = border.width / 40
+            var progress = 0
+            var prevColumns = (Seq[BootRuleColumn](), 0L)
+
+            for (x <- border.minX to border.maxX) {
+                if (progress % widthPart == 0)
+                    print("#")
+
+                val columns = activeSlices.filter(a => x >= a.rect.minX && x <= a.rect.maxX).map(_.toColumn())
+
+                if (columns == prevColumns._1) {
+                    score += prevColumns._2
+                } else {
+                    var columnScore = 0L                        
+                    for (y <- border.minY to border.maxY) {
+                        if (ruleOutcome(y, columns)) {
+                            columnScore += 1
+                        }
+                    }
+
+                    prevColumns = (columns, columnScore)
+                    score += columnScore
+                }
+
+                progress += 1
             }
-            else {
-               
-                0
-            }
+
+            println()
+            println(s"  Score = $score")
+            score
         }
     }
 
@@ -58,8 +81,12 @@ object Main {
         rules.filter(_.contains(x, y, z)).map(_.switchOn).lastOption.getOrElse(false)
     }
 
-    def ruleOutcome(x: Int, y: Int, rules: Seq[BootRuleSlice]): Boolean = {
-        rules.filter(_.contains(x, y)).map(_.switchOn).lastOption.getOrElse(false)
+    def ruleOutcome(x: Int, y: Int, slices: Seq[BootRuleSlice]): Boolean = {
+        slices.filter(_.contains(x, y)).map(_.switchOn).lastOption.getOrElse(false)
+    }
+
+    def ruleOutcome(y: Int, columns: Seq[BootRuleColumn]): Boolean = {
+        columns.filter(_.contains(y)).map(_.switchOn).lastOption.getOrElse(false)
     }
 
     def parse(line: String): BootRule = {
@@ -93,6 +120,22 @@ object Main {
         }
 
         (minX, maxX, minY, maxY, minZ, maxZ)
+    }
+
+    def slicesBorder(slices: Seq[BootRuleSlice]): Rect = {
+        var minX = slices.head.rect.minX
+        var maxX = slices.head.rect.maxX
+        var minY = slices.head.rect.minY
+        var maxY = slices.head.rect.maxY
+        
+        for (slice <- slices.drop(1)) {
+            minX = minX.min(slice.rect.minX)
+            maxX = maxX.max(slice.rect.maxX)
+            minY = minY.min(slice.rect.minY)
+            maxY = maxY.max(slice.rect.maxY)
+        }
+
+        Rect(minX, maxX, minY, maxY)
     }     
 }
 
@@ -103,7 +146,13 @@ case class BootRule(val switchOn: Boolean, val minX: Int, val maxX: Int, val min
 }
 
 case class BootRuleSlice(val switchOn: Boolean, val rect: Rect) {
-     def contains(x: Int, y: Int): Boolean = x >= rect.minX && x <= rect.maxX && y >= rect.minY && y <= rect.maxY    
+     def contains(x: Int, y: Int): Boolean = x >= rect.minX && x <= rect.maxX && y >= rect.minY && y <= rect.maxY
+
+     def toColumn(): BootRuleColumn = BootRuleColumn(switchOn, rect.minY, rect.maxY)
+}
+
+case class BootRuleColumn(val switchOn: Boolean, val minY: Int, val maxY: Int) {
+    def contains(y: Int): Boolean = y >= minY && y <= maxY
 }
 
 case class Rect(val minX: Int, val maxX: Int, val minY: Int, val maxY: Int) {
@@ -111,4 +160,6 @@ case class Rect(val minX: Int, val maxX: Int, val minY: Int, val maxY: Int) {
     def overlapsWith(other: Rect): Boolean = {
         maxX >= other.minX && minX <= other.maxX && maxY >= other.minY && minY <= other.maxY
     }
+
+    def width = maxX - minX + 1
 }
